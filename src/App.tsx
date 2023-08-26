7c58976ff1c1d2373a8db540bf586517cd18cd9b
irasanchez <ira.d.sanchez@gmail.com>
2023-08-26 01:51:04 +0000

    menu test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import viteLogo from './assets/vite.svg'
 import reactLogo from './assets/react.svg'
 import sassLogo from './assets/sass.svg'
 import githubLogo from './assets/github.svg'
+import { Dropdown, Menu, MenuButton, MenuItem } from '@mui/base'
 import './App.scss'
 
 function App() {
@@ -9,6 +10,14 @@ function App() {
     <div className="App">
       <h1>Pockets</h1>
       {/* nav */}
+      <Dropdown>
+        <MenuButton>Dashboard...</MenuButton>
+        <Menu>
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>My account</MenuItem>
+          <MenuItem>Logout</MenuItem>
+        </Menu>
+      </Dropdown>
       {/* page */}
     </div>
   )
