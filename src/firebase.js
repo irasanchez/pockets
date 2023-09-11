5e4fc200341478089be9d076618faa9e2abb9052
irasanchez <ira.d.sanchez@gmail.com>
2023-09-11 22:04:01 +0000

    Adds offline persistence setup via firestore

--- src/firebase.js
+++ src/firebase.js
@@ -7,6 +7,20 @@ var firebaseConfig = {
   projectId: "your_project_id",
 };
 
+// Enable offline persistence
+firebase.firestore().enablePersistence()
+  .catch((err) => {
+      if (err.code === 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a time.
+          console.log('Persistence failed');
+      } else if (err.code === 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          console.log('Persistence is not available in this browser');
+      }
+  });
+
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
